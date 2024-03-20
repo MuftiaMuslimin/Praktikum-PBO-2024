8ea46b3977f9833a61335f8652f0b547f37dbd45
MuftiaMuslimin <145562796+MuftiaMuslimin@users.noreply.github.com>
2024-03-20 21:13:40 +0800

    posttest2

---
+++ POSTTEST2/barangtoko/produkpakaian.java
@@ -0,0 +1,27 @@
+package barangtoko;
+
+public class produkpakaian {
+    private String nama;
+    private double harga;
+
+    public produkpakaian(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+}
